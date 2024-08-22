5709d3fb239eda49ddafe720214d87101acee9d4
tanzheng <tanzheng@tsinghua.edu.cn>
2024-08-22 15:07:00 +0800

    改为绝对路径

--- src/main/java/com/tsinghua/tagsystem/service/impl/ModelServiceImpl.java
+++ src/main/java/com/tsinghua/tagsystem/service/impl/ModelServiceImpl.java
@@ -13,7 +13,7 @@ public class ModelServiceImpl implements ModelService {
     @Override
     public void train() {
         try{
-            String cmd = "cd /home/tz/copy-code/W2NER-main && /home/tz/miniconda3/envs/pufa/bin/python3.8 predict_for_test.py'";
+            String cmd = "/home/tz/miniconda3/envs/pufa/bin/python3.8 /home/tz/copy-code/W2NER-main/predict_for_test.py";
             Process process = Runtime.getRuntime().exec(cmd);
             BufferedReader reader = new BufferedReader(new InputStreamReader(process.getInputStream()));
             String line;
