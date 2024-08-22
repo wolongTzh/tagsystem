d04030a5caf63f837cc6ad73c63893b464eb4294
tanzheng <tanzheng@tsinghua.edu.cn>
2024-08-22 16:11:31 +0800

    尝试更新数据库

--- src/main/java/com/tsinghua/tagsystem/manager/UserManager.java
+++ src/main/java/com/tsinghua/tagsystem/manager/UserManager.java
@@ -4,6 +4,7 @@ import com.baomidou.mybatisplus.core.conditions.query.QueryWrapper;
 import com.baomidou.mybatisplus.extension.service.IService;
 import com.baomidou.mybatisplus.extension.service.impl.ServiceImpl;
 import com.tsinghua.tagsystem.dao.entity.TsUser;
+import com.tsinghua.tagsystem.dao.entity.WorkerTaskRela;
 import com.tsinghua.tagsystem.dao.mapper.UserMapper;
 import org.springframework.stereotype.Component;
 
@@ -35,5 +36,11 @@ public class UserManager extends ServiceImpl<UserMapper, TsUser> implements ISer
         return this.list(new QueryWrapper<TsUser>()
                 .lambda()
                 .eq(TsUser::getIdentity, "worker"));
+    }
+
+    public boolean updateByUserId(TsUser tsUser) {
+        return this.update(tsUser, new QueryWrapper<TsUser>()
+                .lambda()
+                .eq(TsUser::getUserId, tsUser.getUserId()));
     }
 }
--- src/main/java/com/tsinghua/tagsystem/service/impl/AsyncServiceImpl.java
+++ src/main/java/com/tsinghua/tagsystem/service/impl/AsyncServiceImpl.java
@@ -1,7 +1,10 @@
 package com.tsinghua.tagsystem.service.impl;
 
+import com.tsinghua.tagsystem.dao.entity.TsUser;
+import com.tsinghua.tagsystem.manager.UserManager;
 import com.tsinghua.tagsystem.service.AsyncService;
 import com.tsinghua.tagsystem.service.ModelService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.scheduling.annotation.Async;
 import org.springframework.stereotype.Service;
 
@@ -11,6 +14,8 @@ import java.io.InputStreamReader;
 @Service
 public class AsyncServiceImpl implements AsyncService {
 
+    @Autowired
+    UserManager userManager;
     @Override
     public void asyncTrain() {
         System.out.println("开始执行异步方法");
@@ -29,5 +34,9 @@ public class AsyncServiceImpl implements AsyncService {
             e.printStackTrace();
         }
         System.out.println("结束执行异步方法");
+        TsUser tsUser = userManager.getByUserName("谭峥");
+        tsUser.setIdentity("async");
+        userManager.updateByUserId(tsUser);
+        System.out.println("结束执行更细数据库");
     }
 }
