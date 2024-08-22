1cafa386e867595f6b1f416672b2412e9b0ca187
tanzheng <tanzheng@tsinghua.edu.cn>
2024-08-22 15:30:23 +0800

    只在类上加注解会如何

--- src/main/java/com/tsinghua/tagsystem/service/impl/AsyncServiceImpl.java
+++ src/main/java/com/tsinghua/tagsystem/service/impl/AsyncServiceImpl.java
@@ -12,7 +12,6 @@ import java.io.InputStreamReader;
 public class AsyncServiceImpl implements AsyncService {
 
     @Override
-    @Async
     public void asyncTrain() {
         try {
             String cmd = "sh cmd.sh";
