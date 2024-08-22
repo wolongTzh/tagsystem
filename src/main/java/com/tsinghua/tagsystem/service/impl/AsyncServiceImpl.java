f7760573a4bfc1a695f93c50ab33e63813f9468b
tanzheng <tanzheng@tsinghua.edu.cn>
2024-08-22 15:46:12 +0800

    改为绝对路径

--- src/main/java/com/tsinghua/tagsystem/service/impl/AsyncServiceImpl.java
+++ src/main/java/com/tsinghua/tagsystem/service/impl/AsyncServiceImpl.java
@@ -15,9 +15,8 @@ public class AsyncServiceImpl implements AsyncService {
     public void asyncTrain() {
         System.out.println("开始执行异步方法");
         try {
-            String cmd = "sh cmd.sh";
+            String cmd = "sh /home/tz/tagsystem/cmd.sh";
             Process process = Runtime.getRuntime().exec(cmd);
-            System.out.println("调用异步方法之后");
             BufferedReader reader = new BufferedReader(new InputStreamReader(process.getInputStream()));
             String line;
             while ((line = reader.readLine()) != null) {
